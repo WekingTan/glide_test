128fdf48e10a2f61ae40ae7c507620defb5b259e
weking <393205641@qq.com>
2017-09-04 10:30:52 +0800

    添加两种占位图，简单设置缓存机制来显示加载占位图

--- app/src/main/java/com/example/administrator/glidetest/MainActivity.java
+++ app/src/main/java/com/example/administrator/glidetest/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +21,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void loadImage(View view) {
         String url = "http://img.mianbao99.com:88/newpic/201207/04/4ff3f0e439bf3.jpg";
-        Glide.with(this).load(url).into(imageView);
+        Glide.with(this)
+                .load(url)
+                .placeholder(R.drawable.chaoren)                //添加占位图
+                .error(R.drawable.error)                        //添加异常占位图
+                .diskCacheStrategy(DiskCacheStrategy.NONE)      //设置缓存机制
+                .into(imageView);
     }
 }
